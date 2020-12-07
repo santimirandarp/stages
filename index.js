3daac63bef92be1ec28ffbe6fd7b7bb34db2f66b
Santi <santimirandarp@gmail.com>
2020-12-07 21:47:16 +0000

    new branch, new directory structure

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const path = require("path")
 const dbcalls = require("./serverJs/dbcalls")
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname + "/resources/html")))
+app.use(express.static(path.join(__dirname + "/resources/css/")))
 app.use(express.static(path.join(__dirname + "/resources")))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "about.html"))
-})
-
-//let pings = 0
-//appends to response header, to any type /quote request
 app.use((req, res, next) => {
  res.header("Access-Control-Allow-Origin", "*");
  next()
