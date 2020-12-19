7df38e01b8931ea982ee5ed296338318cf8ea699
Santi <santimirandarp@gmail.com>
2020-12-19 14:30:55 +0000

    navbar done

--- resources/js/common.js
+++ resources/js/common.js
@@ -4,28 +4,30 @@ const settings = document.querySelector(".userItem__settings")
 
 const navBar = document.getElementById("navbar");
 const navBarUl = document.querySelector(".navbar__ul");
-const hamburger=document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger");
 const lines = hamburger.children;
 
 const clickBurger = () => {
-if (window.screen.width<750){
+if (window.screen.width<700){
   if(navBar.style.height!=="75vh"){
     navBar.style.height="75vh"
     navBar.style.width="50vw"
+    navBarUl.style.marginTop="4rem"
   } else{
     navBar.style.height="4rem"
     navBar.style.width="4rem"
   }
 } else {
-    navBar.style.height="unset"
-    navBar.style.width="100%"
+    navBar.style.overflow="auto"
+    navBar.style.height="auto"
+    navBar.style.width = "100%"
 }
   lines[0].classList.toggle("hamburger__line1")
   lines[1].classList.toggle("hamburger__line2")
   lines[2].classList.toggle("hamburger__line3")
   return 
 }
-hamburger.addEventListener("click", ()=>clickBurger())
+hamburger.addEventListener("click", ()=> clickBurger())
 hamburger.addEventListener("resize", ()=> clickBurger())
 
 const toggleUserSettings = (element) => {
