c3a99c81313b387b85754f96b732fbbf1996cec6
Santi <santimirandarp@gmail.com>
2020-12-19 13:56:19 +0000

    now on phase 3 navbar. Fixing details

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,7 +1,34 @@
 const account = document.querySelector(".userItem__username")
 const settings = document.querySelector(".userItem__settings")
 
-const toggleDisplay = (element) => {
+
+const navBar = document.getElementById("navbar");
+const navBarUl = document.querySelector(".navbar__ul");
+const hamburger=document.querySelector(".hamburger");
+const lines = hamburger.children;
+
+const clickBurger = () => {
+if (window.screen.width<750){
+  if(navBar.style.height!=="75vh"){
+    navBar.style.height="75vh"
+    navBar.style.width="50vw"
+  } else{
+    navBar.style.height="4rem"
+    navBar.style.width="4rem"
+  }
+} else {
+    navBar.style.height="unset"
+    navBar.style.width="100%"
+}
+  lines[0].classList.toggle("hamburger__line1")
+  lines[1].classList.toggle("hamburger__line2")
+  lines[2].classList.toggle("hamburger__line3")
+  return 
+}
+hamburger.addEventListener("click", ()=>clickBurger())
+hamburger.addEventListener("resize", ()=> clickBurger())
+
+const toggleUserSettings = (element) => {
   if(element.style.display=="block") {
     element.style.display="none"
     element.classList.remove("userItem__settings--height")
@@ -10,5 +37,5 @@ const toggleDisplay = (element) => {
   element.classList.add("userItem__settings--height")
 } }
 
-account.addEventListener("click", ( ) => toggleDisplay(settings))
+account.addEventListener("click", ( ) => toggleUserSettings(settings))
 
